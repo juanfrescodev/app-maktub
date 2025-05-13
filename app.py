2df5f70f542804a898e6dfa2d05909b762c6c7b5
Juan Fresco <juanfresco1@gmail.com>
2025-05-13 12:44:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
-# Muestra el comienzo de la clave
+st.write("Email de cuenta de servicio:")
+st.write(st.secrets["gcp_service_account"]["client_email"])
+
+st.write("Inicio de clave:")
 st.write(st.secrets["gcp_service_account"]["private_key"][:50])
